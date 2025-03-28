8faf81c7c51692cd274230356423cb1bdae82ad3
llydon <llydon@csuchico.edu>
2025-03-28 03:47:25 -0700

    update timestamp

--- intern_project.js
+++ intern_project.js
@@ -5,15 +5,14 @@ const app = express();
 //PortNum is going to be 3000 now for default testing. Might need to make changes for use with Docker.
 const portNum = process.env.PORT || 50404; //Port will dynamically change depending on the assigment from the server.
 
-//Timestamp will use the unix format
-const timeStamp = Math.floor(new Date().getTime() / 1000);
-
-const output = {
-    "message": "My name is Lindsey Lydon",
-    "timestamp": timeStamp
-};
 //Had to move the JSON object to be defined before it is called.
 app.get('/', (req, res) => {
+    //Timestamp will use the unix format
+    const timeStamp = Math.floor(new Date().getTime() / 1000);
+    const output = {
+        "message": "My name is Lindsey Lydon",
+        "timestamp": timeStamp
+    };
     res.set('Content-Type', 'application/json'); //this will change the HTTP header to JSON
     res.json(output);
 });
@@ -24,4 +23,4 @@ app.listen(portNum, '0.0.0.0', () => {console.log('Server is running on port ', 
 //This should listen in at the given port# and then responds with a message to console. 
 
 //Another possible way for the outut to be printed, still think Express is the way to go.
-console.log(JSON.stringify(output));
+//console.log(JSON.stringify(output));
